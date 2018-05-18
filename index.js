b829c5b1d29d8f1253fa6d052678d4620eaf7e72
chandler <dumplings0415@gmail.com>
2018-05-18 08:50:04 +0800

    change tab to space

--- index.js
+++ index.js
@@ -7,22 +7,22 @@ const port = 3000;
 
 app.use(bodyParser());
 app.use(async ctx => {
-	console.log(ctx.url === ctx.request.url);
-	const url = ctx.url;
-	const method = ctx.method;
-	if (url === '/' && method === 'GET') {
-		ctx.body = `<html>
+  console.log(ctx.url === ctx.request.url);
+  const url = ctx.url;
+  const method = ctx.method;
+  if (url === '/' && method === 'GET') {
+    ctx.body = `<html>
 <form action="/" method="post">
 <input type="text" placeholder="name" name="name">
 <input type="text" placeholder="age" name="age">
 <button type="submit">submit</button>
 </form>
 </html>`;
-	} else if (method === 'POST') {
-		ctx.body = ctx.request.body;
-	} else {
-		ctx.body = '404';
-	}
+  } else if (method === 'POST') {
+    ctx.body = ctx.request.body;
+  } else {
+    ctx.body = '404';
+  }
 });
 
 app.listen(port);
